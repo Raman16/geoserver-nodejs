d651928838c72eaa658430b7f2c1247a2dbe1e34
Raman <bandariraman@gmail.com>
2018-04-06 11:15:43 +0530

    connected db

--- model/db-connect.js
+++ model/db-connect.js
@@ -4,8 +4,8 @@ mongoose.Promise=global.Promise;
 const REMOTE_MONGO = 'mongodb://sample:sample@ds129706.mlab.com:29706/sample';
 //const LOCAL_MONGO = 'mongodb://localhost:27017/Geolocation';
 const MONGO_URI = process.env.MONGODB_URI || REMOTE_MONGO;
-mongoose.connect(MONGO_URI)
-.then((res)=>{ console.log('connected')})
+mongoose.connect(REMOTE_MONGO)
+.then((res)=>{ console.log('connected database')})
 .catch((err)=>{ 
    console.log(err);
 });
